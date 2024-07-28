3ef967d1fad6893124df820acb4a782774b91f0a
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-07-28 14:30:12 +0200

    Syncing indicator (#15)

--- modules/lectorium/App.vue
+++ modules/lectorium/App.vue
@@ -2,16 +2,23 @@
   <IonApp>
     <IonRouterOutlet />
     <GlobalAudioPlayer />
+
+    <IonLoading
+      :is-open="sync.inProgress.value && !config.lastSyncedAt.value"
+      message="Loading data..."
+    />
   </IonApp>
 </template>
 
 
 <script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue'
+import { IonApp, IonRouterOutlet, IonLoading } from '@ionic/vue'
 import { GlobalAudioPlayer } from '@lectorium/shared/containers'
+import { useConfig } from '@lectorium/shared/composables'
 import { useSync } from '@lectorium/shared'
 
 const sync = useSync()
+const config = useConfig()
 
 sync.execute({ trackIds: [] })
 </script>
