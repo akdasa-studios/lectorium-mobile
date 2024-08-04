a16bc4b334ec8642de8a765fa14587b42d6313d4
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-08-04 13:23:11 +0200

    feat: background audioplayer service poc

--- modules/lectorium/shared/containers/GlobalAudioPlayer.vue
+++ modules/lectorium/shared/containers/GlobalAudioPlayer.vue
@@ -8,7 +8,8 @@ import { useAudioPlayer } from '@lectorium/shared/composables'
 import { ref, watch } from 'vue'
 import { useMediaControls } from '@vueuse/core'
 import { useUserData } from '@lectorium/playlist'
-import { NativeAudio } from '@capacitor-community/native-audio'
+import { Directory, Filesystem } from '@capacitor/filesystem'
+import { AudioPlayer } from '@core/plugins'
 
 // ── Dependencies ────────────────────────────────────────────────────
 const audioPlayer = useAudioPlayer()
@@ -23,6 +24,7 @@ const { playing, waiting, duration, currentTime } = useMediaControls(audioRef, {
 // ── Hooks ───────────────────────────────────────────────────────────
 watch(audioPlayer.state, async (current, previous) => {
   if (current.trackId !== previous.trackId) {
+    if (previous.trackId) { await onPreviousTrackUnload(previous.trackId) }
     await onTrackChanged(current.trackId, current.position)
   }
   if (current.position !== previous.position) {
@@ -48,32 +50,56 @@ watch(() => audioPlayer.state.value.playing, (current) => {
 })
 
 // ── Handlers ────────────────────────────────────────────────────────
+async function onPreviousTrackUnload(trackId: string) {
+  try {
+    await AudioPlayer.stop({ audioId: trackId })
+    await AudioPlayer.destroy({ audioId: trackId })
+  } catch (error) {
+    // @ts-ignore
+    console.error("Error onPreviousTrackUnload", error.message)
+  }
+}
+
 async function onTrackChanged(
   trackId: string | undefined,
   position: number
 ) {
+  try {
   if (trackId) {
     const track = await library.tracks.get(trackId)
     const media = await userData.media.service.getByUrl(track.url)
 
     if (media?.state === "downloaded") {
-      // url.value = media.localUrl
-      NativeAudio.preload({
-          assetId: "local",
-          assetPath: media.localUrl,
-          audioChannelNum: 1,
-          isUrl: false
-      });
-      NativeAudio.play({
-        assetId: 'local',
-        // time: 6.0 - seek time
-      });
+      const finalPath = (await Filesystem.getUri({
+        directory: Directory.Data,
+        path: media.localPath
+      })).uri
+
+      await AudioPlayer.create({
+        audioId: track.id,
+        useForNotification: true,
+        audioSource: finalPath,
+        friendlyTitle: track.title,
+        isBackgroundMusic: true,
+        loop: false
+      })
+      await AudioPlayer.initialize({
+        audioId: track.id,
+      })
+      await AudioPlayer.play({
+        audioId: track.id,
+      })
+
     } else {
-      url.value = track.url
+      // url.value = track.url
     }
 
     console.log("playing", url.value)
     currentTime.value = position
+  }
+  } catch (error) {
+    // @ts-ignore
+    console.error("Error onTrackChanged", error.message)
   }
 }
 
