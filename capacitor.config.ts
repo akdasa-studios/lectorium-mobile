aba64e7ecb1fd0c32f593605718f72fea30b73d9
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-08-29 20:51:10 +0200

    feat: app brainding

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -11,7 +11,23 @@ const config: CapacitorConfig = {
     CapacitorSQLite: {
       iosDatabaseLocation: 'Library/Database',
       androidDatabaseLocation: 'default',
-    }
+    },
+    SplashScreen: {
+      launchShowDuration: 1500,
+      launchAutoHide: true,
+      launchFadeOutDuration: 500,
+      // backgroundColor: "#5D636F",
+      // androidSplashResourceName: "splash",
+      // androidScaleType: "CENTER_CROP",
+      // showSpinner: true,
+      // androidSpinnerStyle: "large",
+      // iosSpinnerStyle: "small",
+      // spinnerColor: "#999999",
+      // splashFullScreen: true,
+      // splashImmersive: true,
+      // layoutName: "launch_screen",
+      // useDialog: true,
+    },
   }
 };
 
