6037dc018e3f9cad4ec05a2aeaf8cf6e0c5f17f6
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-10-28 12:43:02 +0100

    feat: update downloading artifacts logic

--- utils/download-artifacts.sh
+++ utils/download-artifacts.sh
@@ -1,4 +1,12 @@
 mkdir ./public
-curl $ARTIFACTS_BASE_URL/library-dictionary-v0001.db --output ./public/library-dictionary-v0001.db
-curl $ARTIFACTS_BASE_URL/library-index-v0001.db --output ./public/library-index-v0001.db
-curl $ARTIFACTS_BASE_URL/library-tracks-v0001.db --output ./public/library-tracks-v0001.db
+
+download_artifact() {
+  local artifact_name=$1
+  local output_path=$2
+  local signed_url=$(curl -X POST -H "Content-Type: application/json" -d "{\"object_key\": \"$artifact_name\"}" $SERVER_BASE_URL/signed-url-generator/sign-url | jq -r '.signed_url')
+  curl $signed_url --output $output_path
+}
+
+download_artifact "artifacts/library-dictionary-v0001.db" "./public/library-dictionary-v0001.db"
+download_artifact "artifacts/library-index-v0001.db" "./public/library-index-v0001.db"
+download_artifact "artifacts/library-tracks-v0001.db" "./public/library-tracks-v0001.db"
