ac8eafa530906472ebe7f13ed0c8fd9c32b1f5c7
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-11-02 15:26:52 +0100

    fix: format incomplete dates

--- modules/core/utils/dates.ts
+++ modules/core/utils/dates.ts
@@ -1,14 +1,27 @@
 export function formatDate(
-  input: [number, number, number],
+  input: [number, number?, number?],
   locale: string = 'ru-RU'
 ): string {
-  return new Date(
-    Number(input[0]),
-    Number(input[1])-1,
-    Number(input[2])
-  ).toLocaleDateString(locale, {
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric'
-  })
+  if (input.length === 1) {
+    return input[0].toString()
+  } else if (input.length === 2) {
+    return new Date(
+      Number(input[0]),
+      Number(input[1])-1,
+    ).toLocaleDateString(locale, {
+      year: 'numeric',
+      month: 'short',
+    })
+  } else if (input.length === 3) {
+    return new Date(
+      Number(input[0]),
+      Number(input[1])-1,
+      Number(input[2])
+    ).toLocaleDateString(locale, {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric'
+    })
+  }
+  return ""
 }
